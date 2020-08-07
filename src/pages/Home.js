2ddbba817eaceb501fdd1a48bde632cc3550dbca
L-Villa <63620359+L-Villa@users.noreply.github.com>
2020-08-07 14:18:45 -0700

    Added text content and social links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,8 +10,26 @@ export default function Home() {
             <div className="shoe-image"></div>
             <div className="circle-backdrop"></div>
           </div>
+          <div className="home-landing-social-links">
+            <div className="social-link">FB</div>
+            <div className="social-link">TWI</div>
+            <div className="social-link">INS</div>
+          </div>
         </div>
-        <div className="white-side"></div>
+        <div className="white-side">
+          <div className="white-side-content-container">
+            <div className="white-side-text-container">
+              <p>Running Shoe</p>
+              <h2>Pegasus Turbo 2</h2>
+              <p>
+                Engineered for comfort when running to your next destination.
+                The Pegasus Turbo 2 delivers a powerful combination of support,
+                impact cushioning, and instant responsiveness.
+              </p>
+            </div>
+            <button>Shop Now</button>
+          </div>
+        </div>
       </section>
     </div>
   );
